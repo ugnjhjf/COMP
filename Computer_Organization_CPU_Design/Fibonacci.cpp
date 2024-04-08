3d4bd6983a9f180c1aa023bf815372a378e36b8e
Rokidna G <qq617462323@outlook.com>
2024-04-08 10:27:36 +0800

    20240408_1027 MIPS Fibonacci in CPP

--- Computer_Organization_CPU_Design/Fibonacci.cpp
+++ Computer_Organization_CPU_Design/Fibonacci.cpp
@@ -1,3 +1,17 @@
 #include <iostream>
 
 using namespace std;
+
+int main()
+{
+    int a = 0, b = 1, ans, count;
+    cin >> count;
+    for (int i = 0; i < count; i++)
+    {
+        ans = a + b;
+        a = b;
+        b = ans;
+        cout << ans << " ";
+    }
+    return 0;
+}
