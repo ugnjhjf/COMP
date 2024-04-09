a876a97905a989a5ed09ebc31ee85582c97d5313
Anna MIPS VM <rokidna@outlook.com>
2024-04-09 21:47:49 +0800

    20240409_2147 MIPS Fibonacci Completed

--- Computer_Organization_CPU_Design/MARS_Projects/Assignment_1/Fibonacci_Rokidna.asm
+++ Computer_Organization_CPU_Design/MARS_Projects/Assignment_1/Fibonacci_Rokidna.asm
@@ -14,38 +14,36 @@
     li $v0, 5
     syscall
 
-    #store input into s0
+    #s0 for user input
     move $s0, $v0
 
     #value initialize
-    li $s1,0 #1nd number
-    li $s2,1 #2nd number
-    li $s3,0 #counter
+    li $s1,0 #s1 for 1nd number
+    li $s2,1 #s2 for 2nd number
+    li $s3,0 #s3 for counter
 
     fibonacci:
-    bge $s3,$s0, end
-    add $s4,$s1,$s2 #s4 for current ans
-    move $s1,$s2
-    move $s2,$s4
-    
-    #print result part
-    li $v0, 1
-    move $a0, $s1
-    syscall
-    #print space
-    li $v0, 4
-    la $a0, space
-    syscall
-    
-    
-    addi $s3,$s3,1 #couter++
-    
-    j fibonacci
-
-    #program end
-
-    end:
-      li $v0, 10
+      bge $s3,$s0, end
+      add $s4,$s1,$s2 #s4 for current ans
+      move $s1,$s2
+      move $s2,$s4
+      
+      #print result part
+      li $v0, 1
+      move $a0, $s1
       syscall
+      #print space
+      li $v0, 4
+      la $a0, space
+      syscall
+    
+      addi $s3,$s3,1 #couter++
+      
+      j fibonacci
+
+  #program end
+  end:
+    li $v0, 10
+    syscall
     
   
