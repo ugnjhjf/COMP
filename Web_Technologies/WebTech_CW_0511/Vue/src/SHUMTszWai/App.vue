ce604daf2d9e559b2272698191a66d6268b51d8e
Anna VM1 Web Technologies <rokidna@outlook.com>
2024-05-16 14:20:08 +0800

    Webpack HW finish

--- Web_Technologies/WebTech_CW_0511/Vue/src/SHUMTszWai/App.vue
+++ Web_Technologies/WebTech_CW_0511/Vue/src/SHUMTszWai/App.vue
@@ -1,7 +1,10 @@
 <template>
-  <div id="app">
-    <!-- <img src="./assets/logo.png"> -->
-    <router-view />
+  <div>
+    <div><h1>2159195岑子威 SHUM Tsz Wai</h1></div>
+    <div id="app">
+      <!-- <img src="./assets/logo.png"> -->
+      <router-view />
+    </div>
   </div>
 </template>
 
--- Web_Technologies/WebTech_CW_0511/Vue/webpack.config.js
+++ Web_Technologies/WebTech_CW_0511/Vue/webpack.config.js
@@ -6,7 +6,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 
-    entry: path.resolve(__dirname, './src/main.js'), // 项目入口文件
+    entry: path.resolve(__dirname, './src/SHUMTszWai/main.js'), // 项目入口文件
     output: {
         path: path.resolve(__dirname, './dist'), // 输出目录
         filename: 'SHUMTszWai.js' // 输出文件名
@@ -25,12 +25,12 @@ module.exports = {
             {
                 test: /\.css$/, // 处理 CSS 文件
                 use: [
-                    'style-loader', // 将 CSS 注入到 DOM 中
-                    'css-loader' // 解析 CSS 文件
+                    'style-loader',
+                    'css-loader' 
                 ]
             },
             {
-                test: /\.(png|jpg|gif|svg)$/, // 处理图片文件
+                test: /\.(png|jpg|gif|svg)$/, 
                 loader: 'file-loader',
                 options: {
                     name: '[name].[ext]?[hash]'
@@ -40,16 +40,16 @@ module.exports = {
     },
     plugins: [
     new HtmlWebpackPlugin({
-    template: path.resolve(__dirname, 'index.html'), // 检查这个路径是否正确
+    template: path.resolve(__dirname, './public/SHUMTszWai.html'), 
     filename: 'index.html'
 }),
 
     ],
     resolve: {
         alias: {
-            '@': path.resolve(__dirname, './src'), // 定义别名方便引用模块
-            'vue$': 'vue/dist/vue.esm.js' // 定义别名确保编译模板
+            '@': path.resolve(__dirname, './src'), 
+            'vue$': 'vue/dist/vue.esm.js'
         },
-        extensions: ['.js', '.vue'] // 自动解析确定的扩展
+        extensions: ['.js', '.vue'] 
     }
 };
