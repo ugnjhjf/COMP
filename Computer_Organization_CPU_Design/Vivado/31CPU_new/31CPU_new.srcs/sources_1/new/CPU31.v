296f576c46c0b994d45edfc76c53ab7cf2c40185
Rokidna G <qq617462323@outlook.com>
2024-05-24 09:37:27 +0800

    Write reg output

--- Computer_Organization_CPU_Design/Vivado/31CPU_new/31CPU_new.srcs/sources_1/new/CPU31.v
+++ Computer_Organization_CPU_Design/Vivado/31CPU_new/31CPU_new.srcs/sources_1/new/CPU31.v
@@ -17,24 +17,93 @@ module CPU31(
     output [31:0] ext16_zero_out,
     //RegFIle
     output [4:0] RsC,RtC,RdC,
-    // output [31:0] array_reg_ID_0,array_reg_ID_1,
-    // output [31:0] array_reg_ID_2,array_reg_ID_3,
-    // output [31:0] array_reg_ID_4,array_reg_ID_5,
-    // output [31:0] array_reg_ID_6,array_reg_ID_7,
-    // output [31:0] array_reg_ID_8,array_reg_ID_9,
-    // output [31:0] array_reg_ID_10,array_reg_ID_11,
-    // output [31:0] array_reg_ID_12,array_reg_ID_13,
-    // output [31:0] array_reg_ID_14,array_reg_ID_15,
-    // output [31:0] array_reg_ID_16,array_reg_ID_17,
-    // output [31:0] array_reg_ID_18,array_reg_ID_19,
-    // output [31:0] array_reg_ID_20,array_reg_ID_21,
-    // output [31:0] array_reg_ID_22,array_reg_ID_23,
-    // output [31:0] array_reg_ID_24,array_reg_ID_25,
-    // output [31:0] array_reg_ID_26,array_reg_ID_27,
-    // output [31:0] array_reg_ID_28,array_reg_ID_29,
-    // output [31:0] array_reg_ID_30,array_reg_ID_31
+    output [31:0] array_reg_ID_0,array_reg_ID_1,
+    output [31:0] array_reg_ID_2,array_reg_ID_3,
+    output [31:0] array_reg_ID_4,array_reg_ID_5,
+    output [31:0] array_reg_ID_6,array_reg_ID_7,
+    output [31:0] array_reg_ID_8,array_reg_ID_9,
+    output [31:0] array_reg_ID_10,array_reg_ID_11,
+    output [31:0] array_reg_ID_12,array_reg_ID_13,
+    output [31:0] array_reg_ID_14,array_reg_ID_15,
+    output [31:0] array_reg_ID_16,array_reg_ID_17,
+    output [31:0] array_reg_ID_18,array_reg_ID_19,
+    output [31:0] array_reg_ID_20,array_reg_ID_21,
+    output [31:0] array_reg_ID_22,array_reg_ID_23,
+    output [31:0] array_reg_ID_24,array_reg_ID_25,
+    output [31:0] array_reg_ID_26,array_reg_ID_27,
+    output [31:0] array_reg_ID_28,array_reg_ID_29,
+    output [31:0] array_reg_ID_30,array_reg_ID_31
 
 );
+wire [31:0] reg_0;
+wire [31:0] reg_1;
+wire [31:0] reg_2;
+wire [31:0] reg_3;
+wire [31:0] reg_4;
+wire [31:0] reg_5;
+wire [31:0] reg_6;
+wire [31:0] reg_7;
+wire [31:0] reg_8;
+wire [31:0] reg_9;
+wire [31:0] reg_10;
+wire [31:0] reg_11;
+wire [31:0] reg_12;
+wire [31:0] reg_13;
+wire [31:0] reg_14;
+wire [31:0] reg_15;
+wire [31:0] reg_16;
+wire [31:0] reg_17;
+wire [31:0] reg_18;
+wire [31:0] reg_19;
+wire [31:0] reg_20;
+wire [31:0] reg_21;
+wire [31:0] reg_22;
+wire [31:0] reg_23;
+wire [31:0] reg_24;
+wire [31:0] reg_25;
+wire [31:0] reg_26;
+wire [31:0] reg_27;
+wire [31:0] reg_28;
+wire [31:0] reg_29;
+wire [31:0] reg_30;
+wire [31:0] reg_31;
+
+assign reg_0 = array_reg_ID_0;
+assign reg_1 = array_reg_ID_1;
+assign reg_2 = array_reg_ID_2;
+assign reg_3 = array_reg_ID_3;
+assign reg_4 = array_reg_ID_4;
+assign reg_5 = array_reg_ID_5;
+assign reg_6 = array_reg_ID_6;
+assign reg_7 = array_reg_ID_7;
+assign reg_8 = array_reg_ID_8;
+assign reg_9 = array_reg_ID_9;
+assign reg_10 = array_reg_ID_10;
+assign reg_11 = array_reg_ID_11;
+assign reg_12 = array_reg_ID_12;
+assign reg_13 = array_reg_ID_13;
+assign reg_14 = array_reg_ID_14;
+assign reg_15 = array_reg_ID_15;
+assign reg_16 = array_reg_ID_16;
+assign reg_17 = array_reg_ID_17;
+assign reg_18 = array_reg_ID_18;
+assign reg_19 = array_reg_ID_19;
+assign reg_20 = array_reg_ID_20;
+assign reg_21 = array_reg_ID_21;
+assign reg_22 = array_reg_ID_22;
+assign reg_23 = array_reg_ID_23;
+assign reg_24 = array_reg_ID_24;
+assign reg_25 = array_reg_ID_25;
+assign reg_26 = array_reg_ID_26;
+assign reg_27 = array_reg_ID_27;
+assign reg_28 = array_reg_ID_28;
+assign reg_29 = array_reg_ID_29;
+assign reg_30 = array_reg_ID_30;
+assign reg_31 = array_reg_ID_31;
+
+
+// 实例化CU模块
+
 
 wire [31:0] npc_addr_out, pc_addr_out; // NPC连接PC 和 PC连接NPC + PC连接IMEM
 wire [31:0] instruction, alu_result, imm32;
@@ -272,5 +341,6 @@ assign ext16_signed_out = EXT16signed_to_MUX_9;
 //EXT16 Zero test
 assign ext16_zero_out = EXT16zero_to_MUX_9;
 
+assign array_reg_ID_0 = array_reg_ID_0
 
 endmodule
--- Computer_Organization_CPU_Design/Vivado/31CPU_new/31CPU_new.srcs/sources_1/new/RegFile.v
+++ Computer_Organization_CPU_Design/Vivado/31CPU_new/31CPU_new.srcs/sources_1/new/RegFile.v
@@ -11,6 +11,39 @@ module regfile(                 //寄存器堆RegFile，写入为同步，读取为异步
     output [31:0] Rs_data_out,  //Rs对应的寄存器的输出值
     output [31:0] Rt_data_out,   //Rt对应的寄存器的输出值
 
+        output [31:0] reg_0, 
+    output [31:0] reg_1,
+    output [31:0] reg_2,
+    output [31:0] reg_3,
+    output [31:0] reg_4,
+    output [31:0] reg_5,
+    output [31:0] reg_6,
+    output [31:0] reg_7,
+    output [31:0] reg_8,
+    output [31:0] reg_9,
+    output [31:0] reg_10,
+    output [31:0] reg_11,
+    output [31:0] reg_12,
+    output [31:0] reg_13,
+    output [31:0] reg_14,
+    output [31:0] reg_15,
+    output [31:0] reg_16,
+    output [31:0] reg_17,
+    output [31:0] reg_18,
+    output [31:0] reg_19,
+    output [31:0] reg_20,
+    output [31:0] reg_21,
+    output [31:0] reg_22,
+    output [31:0] reg_23,
+    output [31:0] reg_24,
+    output [31:0] reg_25,
+    output [31:0] reg_26,
+    output [31:0] reg_27,
+    output [31:0] reg_28,
+    output [31:0] reg_29,
+    output [31:0] reg_30,
+    output [31:0] reg_31
+
     );
 /* 内部用变量 */
 reg [31:0] array_reg [31:0];    //定义寄存器堆
@@ -19,6 +52,38 @@ reg [31:0] array_reg [31:0];    //定义寄存器堆
 assign Rs_data_out = ena ? array_reg[RsC] : 32'bz;
 assign Rt_data_out = ena ? array_reg[RtC] : 32'bz;  //只要使能端为高电平（启用寄存器堆）就随时可以读取数据
 
+assign reg_0  = array_reg[0];
+assign reg_1  = array_reg[1];
+assign reg_2  = array_reg[2];
+assign reg_3  = array_reg[3];
+assign reg_4  = array_reg[4];
+assign reg_5  = array_reg[5];
+assign reg_6  = array_reg[6];
+assign reg_7  = array_reg[7];
+assign reg_8  = array_reg[8];
+assign reg_9  = array_reg[9];
+assign reg_10 = array_reg[10];
+assign reg_11 = array_reg[11];
+assign reg_12 = array_reg[12];
+assign reg_13 = array_reg[13];
+assign reg_14 = array_reg[14];
+assign reg_15 = array_reg[15];
+assign reg_16 = array_reg[16];
+assign reg_17 = array_reg[17];
+assign reg_18 = array_reg[18];
+assign reg_19 = array_reg[19];
+assign reg_20 = array_reg[20];
+assign reg_21 = array_reg[21];
+assign reg_22 = array_reg[22];
+assign reg_23 = array_reg[23];
+assign reg_24 = array_reg[24];
+assign reg_25 = array_reg[25];
+assign reg_26 = array_reg[26];
+assign reg_27 = array_reg[27];
+assign reg_28 = array_reg[28];
+assign reg_29 = array_reg[29];
+assign reg_30 = array_reg[30];
+assign reg_31 = array_reg[31];
 /* 接下来考虑异步写入的问题 */
 always @(negedge clk_in or posedge reset)  //复位信号上升沿或时钟下降沿有效
 begin
