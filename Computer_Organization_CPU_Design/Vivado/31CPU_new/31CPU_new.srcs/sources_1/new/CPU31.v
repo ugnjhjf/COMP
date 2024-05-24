bb881f8058c27873fd980fb5bb9ba1985e4743c5
Anna VM2 CPU Design <rokidna@outlook.com>
2024-05-24 12:00:47 +0800

    Add MUX_3

--- Computer_Organization_CPU_Design/Vivado/31CPU_new/31CPU_new.srcs/sources_1/new/CPU31.v
+++ Computer_Organization_CPU_Design/Vivado/31CPU_new/31CPU_new.srcs/sources_1/new/CPU31.v
@@ -29,6 +29,8 @@ wire [31:0] array_reg_ID_9,array_reg_ID_10,array_reg_ID_11,array_reg_ID_12,array
 wire [31:0] array_reg_ID_17,array_reg_ID_18,array_reg_ID_19,array_reg_ID_20,array_reg_ID_21,array_reg_ID_22,array_reg_ID_23,array_reg_ID_24;
 wire [31:0] array_reg_ID_25,array_reg_ID_26,array_reg_ID_27,array_reg_ID_28,array_reg_ID_29,array_reg_ID_30,array_reg_ID_31;
 
+
+
 assign reg_0 = array_reg_ID_0;
 assign reg_1 = array_reg_ID_1;
 assign reg_2 = array_reg_ID_2;
@@ -65,6 +67,7 @@ assign reg_31 = array_reg_ID_31;
 
 // 实例化CU模块
 
+wire [31:0] Rs_to_MUX_3,MUX_2_to_MUX_3;
 
 wire [31:0] npc_addr_out, pc_addr_out; // NPC连接PC 和 PC连接NPC + PC连接IMEM
 wire [31:0] instruction, alu_result, imm32;
@@ -116,6 +119,13 @@ MUX2to1 MUX_2(
     .in1(instruction),
     .sel(CU_to_MUX_2),
     .out(MUX_2_to_MUX_3)
+);
+
+MUX2to1 MUX_3(
+    .in0(MUX_2_to_MUX_3),
+    .in1(Rs_to_MUX_3),
+    .sel(CU_to_MUX_3),
+    .out(MUX_3_to_PC)
 );
 
 // 实例化PC模块
